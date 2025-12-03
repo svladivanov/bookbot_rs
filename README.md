30b80396e322d2445377e4e459f540eaf3dc9134
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-12-03 16:32:54 +0100

    finish project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bookbot
+
+Extremely crappy analysis of long .txt files. Written for absolutely no other reason than it being a way for me to (finally) get into writing Rust.
+
+## Usage
+
+- Get a random book and save it to ./books/<name>.txt: `wget -O books/frankenstein.txt https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/frankenstein.txt`
+- Run the super-ultra-mega-uber-analysis tool: `cargo run -- books/<name>.txt`
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,36 @@ mod stats;
 
 use stats::get_num_chars;
 use stats::get_num_words;
+use std::env;
 use std::fs;
+use std::process::exit;
 
 fn main() {
-    let file_path = String::from("books/frankenstein.txt");
-    let contents = get_book_text(file_path);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: bookbot_rs <file_path> from the root dir");
+        exit(1);
+    }
+    let file_path = &args[1];
+
+    let contents = get_book_text(file_path.clone());
     let num_words = get_num_words(contents.clone());
     let num_chars = get_num_chars(contents);
 
+    let mut sorted_chars: Vec<(&char, &i32)> = num_chars.iter().collect();
+    sorted_chars.sort_by(|a, b| b.1.cmp(a.1));
+
+    println!("============ BOOKBOT ============");
+    println!("Analyzing book found at {file_path}...");
+    println!("----------- Word Count ----------");
     println!("Found {num_words} total words");
-    for (character, count) in num_chars {
-        println!("{}: {}", character, count);
+    println!("--------- Character Count -------");
+    for (character, count) in sorted_chars {
+        if character.is_alphanumeric() {
+            println!("{}: {}", character, count);
+        }
     }
+    println!("============= END ===============");
 }
 
 fn get_book_text(path: String) -> String {
