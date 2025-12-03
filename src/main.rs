e1d7652da7bd510ed0ec8a5353fd4f6d7d07ebb0
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-12-03 12:06:33 +0100

    extract text and count words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = String::from("books/frankenstein.txt");
+    let contents = get_book_text(file_path);
+    let num_words = get_num_words(contents);
+    println!("Found {num_words} total words");
+}
+
+fn get_book_text(path: String) -> String {
+    fs::read_to_string(path).expect("Could not read file at path")
+}
+
+fn get_num_words(text: String) -> usize {
+    text.split_whitespace().count()
 }
